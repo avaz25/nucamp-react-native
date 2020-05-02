cfb266afa58b848334dbf65f285a60245a5b03be
Albert Vazquez <avazquez25@gmail.com>
2020-05-01 21:34:24 -0700

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 // Only grabs a part of the state, instead of unnecessarily grabbing all of it
 const mapStateToProps = state => {
@@ -33,6 +34,27 @@ class About extends Component {
                     subtitle={item.description}
                     leftAvatar={{ source: {uri: baseUrl + item.image}}}
                 />
+            );
+        };
+
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card title="Community Partners">
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card title="Community Partners">
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
             );
         }
         return (
